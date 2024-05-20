ea700219f73f09e8636f7f75fe30a64264949560
Marshanda Gwie <marshandasugiarto@gmail.com>
2024-05-20 21:13:43 +0700

    Initial Commit

---
+++ MyTin/MyTinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTinApp.swift
+//  MyTin
+//
+//  Created by Marshanda Gwie on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
